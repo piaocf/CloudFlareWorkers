ebd2f0cd03e2d3caa87ca17353e63b1240419381
XsAction <82486706+xsaction@users.noreply.github.com>
2021-08-19 13:24:34 +0800

    Create heroku.js

---
+++ heroku.js
@@ -0,0 +1,20 @@
+const SingleDay = '第一个网址'
+const DoubleDay = '第二个网址'
+addEventListener(
+    "fetch",event => {
+    
+        let nd = new Date();
+        if (nd.getDate()%2) {
+            host = SingleDay
+        } else {
+            host = DoubleDay
+        }
+        
+        let url=new URL(event.request.url);
+        url.hostname=host;
+        let request=new Request(url,event.request);
+        event. respondWith(
+            fetch(request)
+        )
+    }
+)
